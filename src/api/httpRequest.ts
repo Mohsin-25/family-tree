99da8e7a2348798ec196ad4518e70ae54e7e7ca4
MohammadMohsin25 <mohsinmohammad122@gmail.com>
2026-02-15 20:06:52 +0530

    feat : added http req handler

---
+++ src/api/httpRequest.ts
@@ -0,0 +1,69 @@
+import axios, { type AxiosResponse } from "axios";
+
+enum httpMethods {
+  get = "GET",
+  post = "POST",
+  patch = "PATCH",
+  put = "PUT",
+  delete = "DELETE",
+}
+
+interface httpRequestProps<T, U> {
+  url: string;
+  method: httpMethods;
+  options?: T;
+  payload?: U;
+  params?: any;
+}
+
+const api = axios.create({
+  baseURL: "https://family-tree-backend-wj7l.onrender.com/api/",
+  headers: {
+    Accept: "application/json, text/plain",
+  },
+});
+
+api.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.request.use((req) => {
+  const token = localStorage.getItem("accessToken");
+  req.headers["Authorization"] = `Bearer ${token}`;
+  return req;
+});
+
+const httpRequest = async <T, U>({
+  url = "",
+  method = httpMethods.get,
+  options,
+  payload,
+  params,
+}: httpRequestProps<T, U>) => {
+  return await api({
+    ...options,
+    url,
+    method: method,
+    data: payload,
+    params: params,
+  })
+    .then((response: AxiosResponse<any, any>) => {
+      return response?.data;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+// const httpRequest = () => {};
+
+const setAxiosAuthHeader = ({ authToken }: { authToken: string }) => {
+  api.defaults.headers.common = { Authorization: `Bearer ${authToken}` };
+};
+
+export { httpRequest, httpMethods, setAxiosAuthHeader };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5000, //5 seconds
+      retry: 2,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
