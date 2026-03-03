360e8c6e66a8026bef81fbbd725181889a635883
MohammadMohsin25 <mohsinmohammad122@gmail.com>
2026-03-03 17:12:12 +0530

    feat : added default route to dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Dashboard from "../modules/dashboard";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <></>;
+  return <Dashboard />;
 }
