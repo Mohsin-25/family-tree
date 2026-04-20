413796285c1a713c66660b5aae0206d75283aab5
Mohammad Mohsin <mohammadmohsin@fyntune.com>
2026-04-20 18:58:48 +0530

    feat : added login redirection animation.

--- src/modules/auth/logIn/services/service.ts
+++ src/modules/auth/logIn/services/service.ts
@@ -1,9 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
 import { httpMethods, httpRequest } from "../../../../api/httpRequest";
 import { useAppToast } from "../../../../components/Toast";
+import { useNavigate } from "@tanstack/react-router";
 
 export const useGetLoggedin = () => {
   const { showToast } = useAppToast();
+  const navigate = useNavigate();
+
   const { mutate, isPending, reset } = useMutation({
     mutationFn: (payload: { userName: string; password: string }) => {
       return httpRequest({
@@ -19,10 +22,37 @@ export const useGetLoggedin = () => {
         status: res?.status,
       });
       if (res?.data?.token) {
+        const img = document.getElementById("login-page-bg");
+        const form = document.getElementById("login-page-form");
+
         setTimeout(() => {
           localStorage.setItem("token", res?.data?.token);
-          window.location.href = "/dashboard";
-        }, 1500);
+
+          form?.classList.add(
+            "opacity-0",
+            "transition-opacity",
+            "duration-2000",
+            "ease-in-out",
+          );
+        }, 500);
+
+        setTimeout(() => {
+          img?.classList.add(
+            "scale-[300]",
+            "transition-scale",
+            "duration-5000",
+            "ease-in-out",
+          );
+        }, 1000);
+
+        setTimeout(() => {
+          navigate({
+            to: "/dashboard",
+            // state: {
+            //   fromLogin: true,
+            // },
+          });
+        }, 3500);
       } else {
         reset();
       }
--- src/modules/newAuth/index.tsx
+++ src/modules/newAuth/index.tsx
@@ -3,21 +3,32 @@ import bgImage from "../../assets/fam-tree.jpg";
 // import bgImage from "../../assets/fam-tree-removed-bg.png";
 import { useState } from "react";
 import NewLoginForm from "../auth/logIn/components/NewLoginForm";
+import { useGetLoggedin } from "../auth/logIn/services/service";
+import { useGetSignin } from "../auth/signIn/services/service";
 
 const NewAuth = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const { mutate, isPending } = useGetLoggedin();
+  const { mutate: signinMutate, isPending: isSigninPending } = useGetSignin({
+    setIsLogin,
+  });
+
   return (
     <div className="flex">
       <div className="w-full h-screen overflow-hidden relative bg-green-100">
         <img
+          id="login-page-bg"
           src={bgImage}
           className="w-full h-full object-cover object-center"
           alt="family-bg"
         />
       </div>
-      <div className=" bg-white/40 rounded-xl flex fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-filter backdrop-blur-[2px] bg-opacity-10">
+
+      <div
+        id="login-page-form"
+        className=" bg-white/40 rounded-xl flex fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-filter backdrop-blur-[2px] bg-opacity-10"
+      >
         <div className="flex flex-col gap-5 items-center justify-center w-[400px] rounded-2xl shadow-xl p-4">
-          {/* HEADER */}
           <div className="flex flex-col items-center justify-center">
             <span className="bg-primary size-10 flex items-center justify-center rounded-full cursor-pointer">
               <TreePalm className="text-white" />
@@ -27,7 +38,7 @@ const NewAuth = () => {
               Discover and preserve your family heritage
             </span>
           </div>
-          {/* FORM */}
+
           <div className="flex flex-col gap-5 border border-black/10 bg-white/70 p-4 rounded-md w-full">
             {isLogin ? (
               <div className="flex flex-col">
@@ -46,7 +57,14 @@ const NewAuth = () => {
                 </span>
               </div>
             )}
-            <NewLoginForm isLogin={isLogin} setIsLogin={setIsLogin} />
+
+            <NewLoginForm
+              isLogin={isLogin}
+              mutate={mutate}
+              isPending={isPending}
+              signinMutate={signinMutate}
+              isSigninPending={isSigninPending}
+            />
 
             {isLogin ? (
               <div className="text-center text-sm">
