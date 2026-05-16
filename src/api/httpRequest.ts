dd89701719d76aabe6da0b646e023ce9e04b5c93
MohammadMohsin25 <mohsinmohammad122@gmail.com>
2026-05-16 23:13:00 +0530

    fix : deployment issue

--- src/api/httpRequest.ts
+++ src/api/httpRequest.ts
@@ -17,8 +17,8 @@ interface httpRequestProps<T, U> {
 }
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/",
-  // "https://family-tree-backend-wj7l.onrender.com/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: "https://family-tree-backend-wj7l.onrender.com/api/",
   headers: {
     Accept: "application/json, text/plain",
   },
